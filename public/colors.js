2cbef0e08efd82123195fb5388e2f65e5d864fbc
d4vss <godflod@gmx.at>
2023-05-09 13:24:10 +0200

    Blue flash removed

--- public/colors.js
+++ public/colors.js
@@ -1,9 +1,11 @@
 addEventListener('load' , () => {
     const div = document.querySelector('.colors');
     const colors = {'accent-blue' : 'rgb(132, 173, 235)', 'accent-gray' : '#2f2f2f', 'accent-red' : '#e67e7e'}
-    if (window.localStorage.getItem('color-accent') != null) {
-        document.documentElement.style.setProperty('--color-accent', window.localStorage.getItem('color-accent'))
+    if (window.localStorage.getItem('color-accent') == null) {
+        window.localStorage.setItem('color-accent', colors['accent-blue'])
     }
+
+    document.documentElement.style.setProperty('--color-accent', window.localStorage.getItem('color-accent'))
 
     div.childNodes.forEach(child => {
         child.addEventListener('click', () => {
